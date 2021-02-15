5fac9d575a7a9b725db42eefa0c409cd67ab367b
RKRedfern <rkredfern@gmail.com>
2021-02-15 14:24:09 -0500

    GET fetch added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-console.log("In Index.js");
+const baseURL = "http://localhost:3000/api/v1/rvnes"
 
+document.addEventListener('DOMContentLoaded', () => {
+    getRvnes()
+})
+
+function getRvnes(){
+    fetch(baseURL)
+    .then(response => response.json())
+    .then(addRvnesToDom)
+}
+
+function addRvnesToDom(rvne){
+    // Find the best way to add to DOM - certainly won't all be within a function?
+}
 
