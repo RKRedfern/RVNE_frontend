9b06d6cbbba693d4161b907912b3fbac2d84a31d
RKRedfern <rkredfern@gmail.com>
2021-02-16 19:25:04 -0500

    messing around

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         newRvneHandler(e))
 })
 
+
 function getRvnes(){
     fetch(baseURL)
     .then(response => response.json())
@@ -45,3 +46,40 @@ function postFetch(content) {
         console.log(rvne);
     })
 }
+
+
+function newUserFormHandler(e) {
+    e.preventDefault()
+    const usernameInput = e.target.querySelector("#").value
+    loginFetch(usernameInput)
+}
+
+function fetchMakeNewUser() {
+
+    let newUserNameValue = document.getElementById("new-user-name").value
+
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            username: newUserNameValue
+        })
+    })
+
+    .then(resp=> resp.json())
+    .then(function(json){
+        if (json["username"][0] ===  "has already been taken") {
+            alert("This username has already been taken");
+        } else {
+            user = new User(json.id, json.username)
+            document.getElementById("container").setAttribute("data-user-id", `${user.id}`);
+        }
+
+    })
+    .then(function(){
+        Order.fetchMakeNewOrder() 
+    })
+};
